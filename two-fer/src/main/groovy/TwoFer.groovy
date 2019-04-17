fd6ba38849368688084e3e45c560a605574f00c4
Uzi Landsmann <uzi.landsmann@bisnode.com>
2019-04-17 21:40:47 +0200

    Updated groovy/twofer

--- two-fer/src/main/groovy/TwoFer.groovy
+++ two-fer/src/main/groovy/TwoFer.groovy
@@ -1,5 +1,5 @@
 class TwoFer {
     static String twoFer(String name = "you") {
-        "One for ${name == "" ? "you" : name}, one for me."
+        "One for ${name ?: 'you'}, one for me."
     }
 }
